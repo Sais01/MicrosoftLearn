998366661c9499ad3893c983b20ccd765687040f
Crisofer Gaier Sais <cristofer.gaier.sais@gmail.com>
2023-03-21 12:18:57 -0300

    first commit

---
+++ instrucaoFor/Program.cs
@@ -0,0 +1,20 @@
+﻿// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine(i);
+//     if (i == 7)
+//     {
+//         Console.WriteLine("chegou no 7");
+//         break;
+//     } 
+// }
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i=0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+}
